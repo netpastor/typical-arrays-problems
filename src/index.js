44cf37f76923c843042f91d03cca8f0ed2b7f7ad
Vadim Shatalov <vadimshatalov@gmail.com>
2020-02-21 00:07:23 +0400

    feat: Solve Typical Arrays Problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    if (!array || ! array.length ) return 0;
+    return Math.min(...array)
+};
 
 exports.max = function max (array) {
-  return 0;
-}
+    if (!array || ! array.length ) return 0;
+    return Math.max(...array)
+};
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    if (!array || ! array.length ) return 0;
+    let sum = array.reduce((previous, current) => current += previous);
+    return sum / array.length;
+};
